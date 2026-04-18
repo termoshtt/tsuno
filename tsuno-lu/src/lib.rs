cd8b4d9dd1457f9af7cf905e727f31b29efe2b52
Toshiki Teramura <toshiki.teramura@gmail.com>
2026-04-18 15:56:52 +0900

    L storage

--- tsuno-lu/src/lib.rs
+++ tsuno-lu/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[katexit::katexit]
+/// Storage for L (lower-triangle) matrix of LU decomposition.
+///
+/// $L$ matrix is represented as a product of unit triangle matrices $L = M_0 M_1 \cdots$,
+/// where each unit triangle matrix is
+///
+/// $$
+/// M_k = 1 - \mu_k |r_k\rangle \langle c_k|, \quad r_k \neq c_k
+/// $$
+///
+/// Invariant
+/// ---------
+/// - $\mu_k \neq 0$
+/// - $r_k \neq c_k$
+/// - Length of `mu`, `col`, and `row` are the same.
+///
+pub struct L {
+    /// Multipliers $\mu_k$
+    pub mu: Vec<f64>,
+    /// Column $c_k$
+    pub col: Vec<usize>,
+    /// Row $r_k$
+    pub row: Vec<usize>,
 }
