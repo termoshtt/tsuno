002949ed3ebe89bd70d64a5131a94a38a67225e6
Toshiki Teramura <toshiki.teramura@gmail.com>
2026-04-18 16:58:07 +0900

    U storage

--- tsuno-lu/src/lib.rs
+++ tsuno-lu/src/lib.rs
@@ -16,9 +16,49 @@
 ///
 pub struct L {
     /// Multipliers $\mu_k$
-    pub mu: Vec<f64>,
+    mu: Vec<f64>,
     /// Column $c_k$
-    pub col: Vec<usize>,
+    col: Vec<usize>,
     /// Row $r_k$
-    pub row: Vec<usize>,
+    row: Vec<usize>,
+}
+
+#[katexit::katexit]
+/// Storage for U (upper-triangle or trapezoidal) matrix of LU decomposition.
+///
+/// $U$ is stored similarly to CSR (Compressed Sparse Row) format, but the non-zero entries are non-contiguous for dynamic update.
+///
+pub struct U {
+    arena: Vec<NonZeroEntry>,
+    rows: Vec<RowPtr>,
+}
+
+/// Pointer of arena for each row in [U].
+///
+/// Since [U] allows non-zero entries to be non-contiguous, we need to store the offset and length of each row in the arena.
+///
+struct RowPtr {
+    offset: usize,
+    length: usize,
+}
+
+/// Non-zero entry in with its column index in [U].
+struct NonZeroEntry {
+    value: f64,
+    col: usize,
+}
+
+#[katexit::katexit]
+/// Storage for LU decomposition of a matrix.
+///
+/// Given matrix $A$ is decomposed into $A = LU$ with [L] and [U] storages.
+/// $A$ can be non-square.
+///
+pub struct LU {
+    l: L,
+    u: U,
+    /// Row permutation for $U$, keeping $PUQ$ upper-triangular or trapezoidal.
+    p: Vec<usize>,
+    /// Column permutation for $U$, keeping $PUQ$ upper-triangular or trapezoidal.
+    q: Vec<usize>,
 }
